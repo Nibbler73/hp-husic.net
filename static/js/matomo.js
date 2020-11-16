b95fb13843f75da9e3eaaa8e7bdc4a97724dabaf
Nibbler73 <github.com@123.husic.de>
2020-11-16 17:30:02 +0100

    add .mobileconfig as download-extension

--- static/js/matomo.js
+++ static/js/matomo.js
@@ -1,5 +1,6 @@
   var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['addDownloadExtensions', "mobileconfig"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
